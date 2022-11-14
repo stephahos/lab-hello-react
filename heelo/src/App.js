4543fb3a8238b5970b5e1dd934eca4cf76806ef9
Stephanie AHossi <ahossi.stephanie@gmail.com>
2022-11-14 14:38:50 +0100

    lab

---
+++ heelo/src/App.js
@@ -0,0 +1,23 @@
+import ironhacklogo1 from './images/ironhacklogo.png'
+import menutop from './images/menutop.png'
+import landingpage1 from './images/landingpage1.png'
+import landingpage2 from './images/landingpage2.png'
+import landingpage3 from './images/landingpage3.png'
+import landingpage4 from './images/landingpage4.png'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+<img src={ironhacklogo1} />
+<img src={menutop} />
+<img src={landingpage1} />
+<img src={landingpage2} />
+<img src={landingpage3} />
+<img src={landingpage4} />
+    </div>
+  );
+}
+
+export default App;
